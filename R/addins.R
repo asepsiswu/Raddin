914bf999b3afa2e5f01610d186a66178c5918af8
asepsiswu <asepsiswu@gmail.com>
2021-09-15 16:57:27 +0800

    add keymaps

--- R/addins.R
+++ R/addins.R
@@ -1,4 +1,7 @@
-Rmd2md <- function(){
+library(purrr)
+library(openxlsx)
+library(data.table)
+Rmd2md <- function() {
   knitr::knit(rstudioapi::getSourceEditorContext()$path)
 }
 
@@ -6,11 +9,11 @@ Rmd2md <- function(){
 hugoRmd <- function() {
   ## helper function
   rmd <- rstudioapi::getSourceEditorContext()$path
-  if (!grepl("rmd",rmd,T)) stop( rmd, " is not a Rmarkdown file. Aborting.", call.=FALSE)
+  if (!grepl("rmd", rmd, T)) stop(rmd, " is not a Rmarkdown file. Aborting.", call. = FALSE)
   library(rmarkdown)
     knitr::opts_knit$set(upload.fun = knitr::image_uri) # modify to produce picture inside markdown file
-  md <- gsub(x=rmd,'rmd','md',T)
-  knitr::knit(rmd,md) # convert Rmarkdown file to md file
+  md <- gsub(x = rmd, "rmd", "md", T)
+  knitr::knit(rmd, md) # convert Rmarkdown file to md file
 }
 
 
